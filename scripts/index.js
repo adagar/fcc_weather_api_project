7595b48289fe959b833de666a84bb22bbc1457ba
Adalberto 'Nick' Garza <adagar121987@gmail.com>
2018-03-31 14:24:15 -0400

    More polish to Web API Proj

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
     console.log("ready!");
-    function setWeatherData(position){
-        $.getJSON("https://fcc-weather-api.glitch.me/api/current?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude, function(json){
+    //Setting temperature units
+    var tempUnit = "C";
+
+    var lat;
+    var long;
+
+    function setLocation(position){
+        lat = position.coords.latitude;
+        long = position.coords.longitude;
+        displayWeather();
+    }
+
+    function displayWeather(){
+        $.getJSON("https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + long, function(json){
         //$("#weather").html(JSON.stringify(json));
         
         var html = "";
@@ -9,31 +21,40 @@ $(document).ready(function() {
         //json.forEach(function(val){
         var keys = Object.keys(json);
 
-        //Setting temperature units
-        var tempUnit = "&deg;C"
-        var currTemp = json["main"]["temp"];
+        if(tempUnit == "C"){
+          var currTemp = json["main"]["temp"];
+        } else{
+          var currTemp = ((Number(json["main"]["temp"]) * 9) / 5) + 32;
+        }
+
 
         //Print importnat weather data
         html += "<div class = 'weather-data'>";
         html += "<p id='location'>" + json["name"] + ", " + json["sys"]["country"] + "</p>";
-        html += "<p id = 'temp'> Current Temperature: " + currTemp + tempUnit;
+        html += "<p id = 'temp'> Current Temperature: " + currTemp + "&deg;" + tempUnit;
         html += "<p id = 'cloudCov'> Cloud Cover: " + json["weather"][0]["main"] + "</p>";
         html += "<img src = '" + json["weather"][0]["icon"] + "'>"; //TODO: Integrate these icons https://darkskyapp.github.io/skycons/
 
         html += "</div><br>";
         $("#weather-data").html(html);  
       });
+    } 
 
-    //});
-  } 
-  
+    $("#temp-select input:radio").click(function(){
+      tempUnit = $('input[name=weather-units]:checked').val();
+      alert("Changing temp units to: " + tempUnit);
+      displayWeather();
+    });
+
     function getLocation(){
       if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(setWeatherData);
+        navigator.geolocation.getCurrentPosition(setLocation);
       }else{
         $("#weather").html("Geolocation is not supported by this browser, but I hope its nice out.");
       }
     }
+
+    
+
     getLocation();
-    console.log("Still ready!");
 });
